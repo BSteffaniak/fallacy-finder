5e398f372dbc838e33f03fdd95e91a36cce5aaf4
Braden Steffaniak <braden@novex.tech>
2021-09-05 00:17:47 -0400

    Remove all racist and oppressive references to master in the example input string

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { initDictionaryFromJson } from "./dictionary.ts";
 import { isMatch } from "./matcher.ts";
 import { parseSentence } from "./word-parser.ts";
 
-const input = Deno.args[0] || `The word "master" is always racist. Therefore, we must obliterate it.`;
+const input = Deno.args[0];
+
+if (!input) {
+  console.error("Please provide a string argument.");
+  Deno.exit(1);
+}
+
+const PATTERNS = [
+  "N*? N:UNI|N:UNINEG N",
+  "N*? V"
+];
 
 (async () => {
   await initDictionaryFromJson();
@@ -13,5 +23,11 @@ const input = Deno.args[0] || `The word "master" is always racist. Therefore, we
     .map(s => s.trim())
     .map(s => parseSentence(s));
 
-  console.log(sentences, isMatch(sentences[0], "N*? N:UNI|N:UNINEG N"));
+  sentences.forEach((sentence) => {
+    console.log(sentence);
+
+    PATTERNS.forEach((pattern) => {
+      console.log(pattern, isMatch(sentence, pattern));
+    });
+  });
 })();
